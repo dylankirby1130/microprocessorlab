ae5241dddf888178fcd0580b7d2ed7b676ffd863
dylankirby1130 <bigfatuwus1130@gmail.com>
2025-10-03 18:49:33 -0400

    Adding code outline of first project

--- lpcxpresso55s69_led_blinky_lpc_cm33_core0/Debug/source/semihost_hardfault.d
+++
@@ -1,4 +0,0 @@
-source/semihost_hardfault.o source/semihost_hardfault.d: \
- ../source/semihost_hardfault.c \
- C:\MCUExpresso\ files\lpcxpresso55s69_led_blinky_lpc_cm33_core0\source\mcux_config.h
-C:\MCUExpresso\ files\lpcxpresso55s69_led_blinky_lpc_cm33_core0\source\mcux_config.h:
--- lpcxpresso55s69_led_blinky_lpc_cm33_core0/Debug/startup/boot_multicore_slave.d
+++
@@ -1,4 +0,0 @@
-startup/boot_multicore_slave.o startup/boot_multicore_slave.d: \
- ../startup/boot_multicore_slave.c \
- C:\MCUExpresso\ files\lpcxpresso55s69_led_blinky_lpc_cm33_core0\source\mcux_config.h
-C:\MCUExpresso\ files\lpcxpresso55s69_led_blinky_lpc_cm33_core0\source\mcux_config.h:
